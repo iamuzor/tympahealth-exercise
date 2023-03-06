bcf79c30ccee90f5f04a7c430c52da5b9887b7b7
uzor <iamuzor@gmail.com>
2023-03-06 13:49:38 +0000

    retrieves devices from api and populates table

--- frontend/src/components/DeviceTable.vue
+++ frontend/src/components/DeviceTable.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">Brand</th>
-        <th class="text-left">Model</th>
-        <th class="text-left">OS</th>
-        <th class="text-left">Release Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="device in devices">
-        <td>{{ device.brand }}</td>
-        <td>{{ device.model }}</td>
-        <td>{{ device.os }}</td>
-        <td>{{ device.release_date }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <div>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Brand</th>
+          <th class="text-left">Model</th>
+          <th class="text-left">OS</th>
+          <th class="text-left">Release Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="device in devices">
+          <td>{{ device.brand }}</td>
+          <td>{{ device.model }}</td>
+          <td>{{ device.os }}</td>
+          <td>{{ device.release_date }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -39,10 +41,32 @@ export default {
   methods: {
     async loadDevices() {
       try {
-        const response = await fetch("http://localhost:8080/devices");
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_API_BASE_URL}/devices`
+        );
         const { data } = await response.json();
 
         this.devices = data;
+      } catch (error) {
+        alert("Unable to load devices at the moment. Try again later");
+      }
+    },
+    async createDevice() {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_API_BASE_URL}/devices`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.form),
+          }
+        );
+
+        const { data } = await response.json();
+
+        this.devices.push(data);
       } catch (error) {
         alert("Unable to load devices at the moment. Try again later");
       }
